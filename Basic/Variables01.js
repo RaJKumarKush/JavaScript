ad9316da08e5c4298a5c927bc3f2ca4444e0adfe
Rajkumar Kushwaha <166651520+RaJKumarKush@users.noreply.github.com>
2025-03-30 21:48:18 +0000

    New

---
+++ Basic/Variables01.js
@@ -0,0 +1,15 @@
+const AccountId= 13566
+let AccountEmail = "rkj124@gmail.com"
+var AccountPassword = "12345"
+AccountCity = "Jaipur"
+
+//AccountId = 123  // Not Allowed
+
+AccountEmail = "hdfre@gmail.com"
+AccountPassword = "33452"
+AccountCity = "Hyderabad"
+let AccountState;
+
+console.log(AccountId);
+
+console.table([AccountId,AccountEmail,AccountPassword,AccountCity,AccountState])
