c03f4836605259f6bb9c871bff8526a8a2301349
Rajkumar Kushwaha <166651520+RaJKumarKush@users.noreply.github.com>
2025-03-30 21:55:36 +0000

    Updated

--- Basic/DataTypes01.js
+++ Basic/DataTypes01.js
@@ -1,3 +1,14 @@
 "use strict";
 
 console.log(3+3)
+
+let name = "Raj"
+let age =18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/flase
+// null =>standalone value
+// undefined =>
